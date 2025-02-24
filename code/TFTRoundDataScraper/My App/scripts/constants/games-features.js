cb2d06a9db6eaad0f3fed5fab5390cca6ad9ceae
PrelaunchZeus2 <jfsalmoniv@gmail.com>
2025-02-24 15:31:05 -0800

    added game feature.js

--- code/TFTRoundDataScraper/My App/scripts/constants/games-features.js
+++ code/TFTRoundDataScraper/My App/scripts/constants/games-features.js
@@ -4,24 +4,12 @@ export const kGamesFeatures = new Map([
   [
     54261,
     [
-      'live_client_data',
-      'matchState',
-      'match_info',
-      'death',
-      'respawn',
-      'abilities',
-      'kill',
-      'assist',
-      'gold',
-      'minions',
-      'summoner_info',
-      'gameMode',
-      'teams',
-      'level',
-      'announcer',
-      'counters',
-      'damage',
-      'heal'
+      'Roster',
+      'Store',
+      'Board',
+      'Bench',
+      'Carousel',
+      'Augments'
     ]
   ],
 ]);
